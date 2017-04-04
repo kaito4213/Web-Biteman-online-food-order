4193b87c0a19a9848f8c6e585b87f989cb7602fe
jlou <jlou@wpi.edu>
2017-04-03 22:31:16 -0400

    fixed click bug

--- src/MyOrder.jsx
+++ src/MyOrder.jsx
@@ -1,76 +1,96 @@
 import React from 'react';
 
-var ORDER = [
-    {oid: '1', rid: '1', price: '$49.99', quantity: '1', dname: 'Pad Thai'},
-    {oid: '2', rid: '1', price: '$49.99', quantity: '1', dname: 'crab rangoon'},
-    {oid: '3', rid: '1', price: '$49.99', quantity: '1', dname: 'downpling'},
-    {oid: '4', rid: '1', price: '$49.99', quantity: '1', dname: 'noodle'},
-    {oid: '5', rid: '1', price: '$49.99', quantity: '1', dname: 'fried rice'},
-    {oid: '6', rid: '1', price: '$49.99', quantity: '1', dname: 'chicken wings'}
+let order = [
+  {oid: '1', rid: '1', price: '$49.99', quantity: '1', dname: 'Pad Thai'},
+  {oid: '2', rid: '1', price: '$49.99', quantity: '1', dname: 'crab rangoon'},
+  {oid: '3', rid: '1', price: '$49.99', quantity: '1', dname: 'downpling'},
+  {oid: '4', rid: '1', price: '$49.99', quantity: '1', dname: 'noodle'},
+  {oid: '5', rid: '1', price: '$49.99', quantity: '1', dname: 'fried rice'},
+  {oid: '6', rid: '1', price: '$49.99', quantity: '1', dname: 'chicken wings'}
 ];
 
-var totalPrice = 0
+let totalPrice = 0;
 
 class MyOrder extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {order: {ORDER}}
-        //this.handleClick = this.handleClick.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {order: order};
+  }
 
-    handleClick(e) {
-        var newOrder = []
-        this.state.order.forEach(function(row) {
-            if(row.oid != e.target.value){
-               newOrder.push(row)
-            }
+  deleteOrder(orderId) {
+    debugger;
+    let order = [];
+    this.state.order.forEach(function (row) {
+      if (row.oid != orderId) {
+        order.push(row);
+      }
 
-            this.setState({order: {newOrder}})
-        })
+      this.setState({order});
+    }.bind(this))
 
-    }
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>MY Order</h1>
-                <OrderTable order = {this.state.order} />
-            </div>
+  render() {
+    return (
+      <div>
+        <h1>MY Order</h1>
+        <table>
+          <OrderTable order={this.state.order} deleteOrder={this.deleteOrder.bind(this)}/>
+        </table>
+      </div>
 
-        )
-    }
+    )
+  }
 }
 
 //define info in each order row shown on webpage
 class OrderTable extends React.Component {
-    render() {
-        var orderRow = []
-        this.props.order.forEach(function(order) {
-            orderRow.push(<OrderRow order={order} key = {order.oid} />)
-        })
 
-        return(
-            <tbody>{orderRow}</tbody>
-        )
-    }
+  constructor(props) {
+    super(props);
+  }
+
+  deleteOrder(orderId) {
+    this.props.deleteOrder(orderId);
+  }
+
+  render() {
+    let orderRow = [];
+    this.props.order.forEach(function (order) {
+      orderRow.push(<OrderRow order={order} key={order.oid} deleteOrder={this.deleteOrder.bind(this)}/>)
+    }.bind(this));
+
+    return (
+      <tbody>{orderRow}</tbody>
+    )
+  }
 }
 
 //
 class OrderRow extends React.Component {
-    render() {
-        return(
-            <div>
-                <div>
-                    <h3>{this.props.order.dname}
-                        <span> {this.props.order.price}</span>
-                        <button type="button" onClick = {this.handleClick}
-                                value = {this.props.order.id}>delete</button>
-                    </h3>
-                </div>
-                <hr></hr>
-            </div>
-        )
-    }
+
+  constructor(props) {
+    super(props);
+  }
+
+  handleDeleteOrder(e) {
+    let orderId = e.target.value;
+    this.props.deleteOrder(orderId);
+  }
+
+  render() {
+    return (
+      <tr>
+        <td>
+          <h3>{this.props.order.dname}
+            <span> {this.props.order.price}</span>
+            <button type="button" value={this.props.order.oid} onClick={this.handleDeleteOrder.bind(this)}>Delete
+            </button>
+          </h3>
+        </td>
+      </tr>
+    )
+  }
 }
 
 export default MyOrder;
